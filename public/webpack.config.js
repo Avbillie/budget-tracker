39c189c51b228b2eab3e2ff1c85f7661624ead9a
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2020-12-15 22:41:04 -0500

    initial upload

---
+++ public/webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./assets/js/app.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files must end in ".js" to be transpiled
+        exclude: /node_modules/, // don't transpile code from "node_modules"
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      filename: "manifest.json",
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An application for tracking user budgets",
+      background_color: "#ffffff",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("assets/images/icons/icon-192x192.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("public", "icons")
+        }
+      ]
+    })
+  ],
+};
+
+module.exports = config;
